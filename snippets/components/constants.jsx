54c3dd04cabb2bb3a0c5930c87b131faefbc68e8
Benjamin Smidt <ben@miruml.com>
2025-11-25 14:03:26 -0800

    feat: add pictures to concepts

---
+++ snippets/components/constants.jsx
@@ -0,0 +1,4 @@
+// Shared constants for documentation components
+export const CONCEPT_BORDER_WIDTH = "6px";
+export const CONCEPT_INNER_RADIUS = "10px";
+export const CONCEPT_OUTER_RADIUS = "10px";
