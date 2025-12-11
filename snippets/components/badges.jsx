d8ad67ed9a59ec88a60268a908fc9361950ff857
Benjamin Smidt <ben@miruml.com>
2025-12-10 18:28:13 -0800

    refactor: lots of edits - my bad

--- snippets/components/badges.jsx
+++ snippets/components/badges.jsx
@@ -1,7 +1,31 @@
+export const NullableBadge = ({ size = "sm" }) => {
+    return (
+        <Tooltip tip="Property does not need to be set">
+            <Badge icon="circle" color="gray" size={size}>nullable</Badge>
+        </Tooltip>
+    );
+};
+
+export const EditableBadge = ({ size = "sm" }) => {
+    return (
+        <Tooltip tip="Property can be directly modified">
+            <Badge icon="pencil" color="blue" size={size}>editable</Badge>
+        </Tooltip>
+    );
+};
+
 export const MutableBadge = ({ size = "sm" }) => {
-    return <Badge icon="pencil" color="orange" size={size}>mutable</Badge>;
+    return (
+        <Tooltip tip="Property is automatically updated by the system; cannot be modified directly">
+            <Badge icon="feather" color="orange" size={size}>mutable</Badge>
+        </Tooltip>
+    );
 };
 
 export const ImmutableBadge = ({ size = "sm" }) => {
-    return <Badge icon="lock" color="gray" size={size}>immutable</Badge>;
+    return (
+        <Tooltip tip="Property cannot be modified">
+            <Badge disabled icon="lock" color="gray" size={size}>immutable</Badge>
+        </Tooltip>
+    );
 };
