96a91122a999783cf317ea768a272d7ee467c84e
Benjamin Smidt <ben@miruml.com>
2025-12-10 19:04:43 -0800

    feat: devices 'learn' section is complete

---
+++ snippets/components/releases/cus-val.jsx
@@ -0,0 +1,5 @@
+export const CUSTOM_VALIDATION_TOOLTIP = {
+    tip: "An endpoint that validates configs adhere to the schemas in a release.",
+    cta: "Learn more",
+    href: "/docs/learn/releases/custom-validation"
+};
